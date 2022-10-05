e1afee4a6b8edd0200401e59500c759dff83e16d
Kamil Poręba <kamilporeba@hotmail.com>
2022-10-05 20:43:15 +0200

    input component prepared

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,3 +1,26 @@
-export const Input = () => {
-  return <div>Input</div>
+import { TextField, TextFieldProps } from "@mui/material"
+import { Control, Controller, FieldPath, FieldValues } from "react-hook-form"
+
+type InputProps<TFieldValues extends FieldValues = FieldValues> = {
+  name: FieldPath<TFieldValues>
+  control: Control<TFieldValues>
+  textFieldProps: TextFieldProps
 }
+
+export const Input = <TFieldValues extends FieldValues = FieldValues>({
+  control,
+  name,
+}: InputProps<TFieldValues>) => (
+  <Controller
+    name={name}
+    control={control}
+    render={() => (
+      <TextField
+        name="test"
+        id="outlined-basic"
+        label="Outlined"
+        variant="outlined"
+      />
+    )}
+  />
+)
