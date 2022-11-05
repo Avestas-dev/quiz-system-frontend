1bb1caf4f140f1ca039ea4c2709dc209194554f5
Michal <m.rawski098@gmail.com>
2022-11-05 18:36:34 +0100

    create quiz fix

--- src/sites/trainings/CreateTraining.tsx
+++ src/sites/trainings/CreateTraining.tsx
@@ -1,134 +1,131 @@
-import { yupResolver } from "@hookform/resolvers/yup"
-import { Add } from "@mui/icons-material"
-import { Button } from "@mui/material"
-import { useForm } from "react-hook-form"
-import { InputControl } from "../../components/InputControl"
+import {
+  Box,
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+} from "@mui/material"
 import { Layout } from "../../components/layout/Layout"
-import yup from "../../consts/yupLocaleEN"
+import MovieCreation from "@mui/icons-material/MovieCreation"
+import AudioFile from "@mui/icons-material/AudioFile"
+import Image from "@mui/icons-material/Image"
 
 export const CreateTraining = () => {
-  const addQuestionValidationSchema = yup.object().shape({
-    content: yup.string().required().email(),
-  })
-
-  type AddQuestionFormProps = {
-    question: string
-    answer: string
-  }
-
-  var questions = [
-    { content: "question1 dfkfmslgfdsg", answers: ["answer1", "answer2"] },
-    { content: "question2", answers: [] },
-    { content: "question3", answers: [] },
-    { content: "question4", answers: [] },
-  ]
-
-  const { control, handleSubmit, watch } = useForm<AddQuestionFormProps>({
-    resolver: yupResolver(addQuestionValidationSchema),
-    reValidateMode: "onChange",
-  })
-
   return (
-    <div className="flex">
-      <Layout>
-        <div className="flex flex-row shadow-2xl border-gray border-2 min-h-full">
-          <div className="flex-1 w-[50%] min-h-full ">
-            <div className="flex max-w-[70%]  ml-auto mr-auto justify-center flex-col ">
-              <div className="flex flex-col space-y-4 ">
-                <div className="flex pt-10">
-                  <h1>Create training</h1>
-                </div>
-
-                <InputControl
-                  control={control}
-                  name="question"
-                  label="Quiz name"
-                  autoFocus
-                  inputProps={{ inputMode: "text" }}
-                  defaultValue=""
-                />
-                <div className="flex pt-2">
-                  <h1>Accesibility</h1>
-                </div>
-                <div className="flex">
-                  <input
-                    type="checkbox"
-                    name="accesibility"
-                    id="accesibility"
-                  />
-                  <p className="pl-2">Private</p>
-                </div>
-                <div className="flex">
-                  <input
-                    type="checkbox"
-                    name="accesibility"
-                    id="accesibility"
-                  />
-                  <p className="pl-2">Public</p>
-                </div>
+    <Layout>
+      <div className="flex flex-col h-screen items-center">
+        <div className="flex mt-8 flex-col  space-y-8 p-8 rounded-2xl  w-[60%] h-[80%] bg-yellow-300">
+          <div className="flex flex-row space-x-2  ">
+            <div className="flex flex-col w-1/5 space-y-2  items-center">
+              <div className="flex flex-col items-center rounded-xl bg-yellow-200 p-1 w-3/5">
+                <MovieCreation />
+                <p>Wideo</p>
+              </div>
+              <div className="flex flex-col items-center rounded-xl bg-yellow-200 p-1 w-3/5">
+                <AudioFile />
+                <p>Audio</p>
+              </div>
+              <div className="flex flex-col items-center rounded-xl bg-yellow-200 p-1 w-3/5">
+                <Image />
+                <p>Obraz</p>
               </div>
             </div>
+            <div className="w-4/5 rounded-xl border-4 border-yellow-200">
+              <input
+                placeholder="Wprowadź swoje pytanie tutaj"
+                id="large-input"
+                className="placeholder:text-black placeholder:text-center bg-yellow-300 rounded-xl  w-full h-full"
+                type="text"
+              />
+            </div>
           </div>
-          <div className="flex-1 w-[50%] min-h-full ">
-            <div className="flex max-w-[90%]  ml-auto mr-auto justify-center flex-col ">
-              <div className="flex flex-col space-y-4 ">
-                <div className="flex pt-10">
-                  <h1>Questions:</h1>
-                </div>
-                <InputControl
-                  control={control}
-                  name="question"
-                  label="Question"
-                  autoFocus
-                  inputProps={{ inputMode: "text" }}
-                  defaultValue=""
-                />
-              </div>
-              <div className="flex justify-center pt-4 pb-4">
-                <Button
-                  type="submit"
-                  variant="contained"
-                  style={{ borderRadius: 9999 }}
-                  startIcon={<Add />}
-                >
-                  Add Question
-                </Button>
-              </div>
-              <ul className="list-decimal">
-                {questions.map((question) => (
-                  <div>
-                    <li className="font-bold mb-2">
-                      {question.content}
-                      <Button color="success">Add Answer</Button>
-                      <Button >Edit</Button>
-                      <Button color="error">Delete</Button>
-                      <input
-                        type="checkbox"
-                        name="agree"
-                        id="agree"
-                        className="peer"
-                      />
-                    </li>
-                    <div className="pl-2">
-                      {question.answers.map((answers) => (
-                        <>
-                          <div className="flex mb-2 items-center border-2 ">
-                            <div className="w-full text-grey-darkest">
-                              {answers}
-                            </div>
-                            <Button>Edit</Button>
-                            <Button>Remove</Button>
-                          </div>
-                        </>
-                      ))}
-                    </div>
-                  </div>
-                ))}
-              </ul>
+          <div className="flex flex-row space-x-8">
+            <div className="flex w-1/4 ">
+              <textarea
+                placeholder="Wpisz swoją odpowiedź tutaj"
+                className="placeholder:text-black placeholder:text-center rounded-xl bg-purple-500 w-full h-full"
+                cols={30}
+                rows={10}
+              ></textarea>
+            </div>
+            <div className="flex w-1/4 ">
+              <textarea
+                placeholder="Wpisz swoją odpowiedź tutaj"
+                className="placeholder:text-black placeholder:text-center rounded-xl bg-green-500 w-full h-full"
+                cols={30}
+                rows={10}
+              ></textarea>
+            </div>
+            <div className="flex w-1/4 ">
+              <textarea
+                placeholder="Wpisz swoją odpowiedź tutaj"
+                className="placeholder:text-black placeholder:text-center rounded-xl bg-blue-500 w-full h-full"
+                cols={30}
+                rows={10}
+              ></textarea>
+            </div>
+            <div className="flex w-1/4 ">
+              <textarea
+                placeholder="Wpisz swoją odpowiedź tutaj"
+                className="placeholder:text-black placeholder:text-center rounded-xl bg-red-500 w-full h-full"
+                cols={30}
+                rows={10}
+              ></textarea>
             </div>
           </div>
         </div>
-      </Layout>
-    </div>
+        <div className="flex flex-row w-[60%] space-x-2 mt-2">
+          <h1>tagi:</h1>
+          <Box sx={{ minWidth: 240 }}>
+            <FormControl size="small" fullWidth>
+              <InputLabel id="demo-simple-select-label">Typ</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value=""
+                label="Age"
+                onChange={() => {}}
+              >
+                <MenuItem value={10}>Wielokrotnego wyboru</MenuItem>
+                <MenuItem value={10}>Jednokrotnego wyboru</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+          <Box sx={{ minWidth: 180 }}>
+            <FormControl size="small" fullWidth>
+              <InputLabel id="demo-simple-select-label">Czas</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value=""
+                label="Age"
+                onChange={() => {}}
+              >
+                <MenuItem value={10}>Czas: 30 sekund</MenuItem>
+                <MenuItem value={10}>Czas: 60 sekund</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+          <Box sx={{ minWidth: 120 }}>
+            <FormControl size="small" fullWidth>
+              <InputLabel id="demo-simple-select-label">Temat</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value=""
+                label="Age"
+                onChange={() => {}}
+              >
+                <MenuItem value={10}>Temat1</MenuItem>
+                <MenuItem value={10}>Temat2</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+          <Button>Anuluj</Button>
+          <Button>Zapisz</Button>
+        </div>
+      </div>
+    </Layout>
   )
 }
