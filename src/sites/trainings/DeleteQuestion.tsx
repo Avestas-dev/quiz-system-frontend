b8e01c13849fe85eca42291e7586f9055642d600
Michal <m.rawski098@gmail.com>
2022-12-07 15:24:20 +0100

    working on training session

---
+++ src/sites/trainings/DeleteQuestion.tsx
@@ -0,0 +1,47 @@
+import DeleteOutlined from "@mui/icons-material/DeleteOutlined"
+import { IconButton } from "@mui/material"
+import axios from "axios"
+import { QueryClient, useMutation, useQueryClient } from "react-query"
+import { useNavigate } from "react-router"
+import { toast } from "react-toastify"
+import { Api } from "../../models/Api"
+
+interface DeleteQuestionProps {
+  questionId: string
+}
+
+export const DeleteQuestion = ({ questionId }: DeleteQuestionProps) => {
+  const queryClient = useQueryClient()
+  const navigate = useNavigate()
+
+  const deleteMutation = useMutation<any, any, DeleteQuestionProps>(
+    async ({ questionId }) => {
+      const res = await axios.delete(`/question/${questionId}`)
+      return res.data
+    },
+
+    {
+      onSuccess: async (response) => {
+        toast.success("Delete succesfully!", { autoClose: 2000 })
+        queryClient.invalidateQueries("/questions/all")
+      },
+      onError: (error) => {
+        toast.error(error?.response?.data?.message || "Delete error.", {
+          autoClose: 2000,
+        })
+      },
+    }
+  )
+
+  return (
+    <div className="bg-gray-300 text-[10px] flex flex-row p-1 rounded space-x-2 ">
+      <IconButton
+        onClick={() => {
+          deleteMutation.mutate({ questionId })
+        }}
+      >
+        <DeleteOutlined fontSize="small" />
+      </IconButton>
+    </div>
+  )
+}
--- src/sites/trainings/StartTrainingSession.tsx
+++ src/sites/trainings/StartTrainingSession.tsx
@@ -1,6 +1,41 @@
+import axios from "axios"
+import { useEffect } from "react"
+import { useMutation, useQuery } from "react-query"
+import { useParams } from "react-router"
+import { toast } from "react-toastify"
 import { EditTrainingTopBar } from "./components/EditTrainingTopBar"
 
+export interface TrainingSessionProps {
+  trainingId: number
+}
+
 export const StartTrainingSession = () => {
+  const { id } = useParams()
+
+  const endTraining: TrainingSessionProps = { trainingId: Number(id) }
+
+  useEffect(() => {
+    const { data } = useQuery<any, any, any>(
+      "/training-session/end",
+      async () => {
+        const res = await axios.post("/training-session/end", endTraining)
+        return res.data
+      },
+      {
+        onSuccess: async (response) => {
+          //toast.success("Added training succesfully!", { autoClose: 2000 })
+        },
+        onError: (error) => {
+          toast.error(error?.response?.data?.message || "Add question error.", {
+            autoClose: 2000,
+          })
+        },
+      }
+    )
+  }, [])
+
+  console.log(id)
+
   return (
     <div>
       <EditTrainingTopBar />
