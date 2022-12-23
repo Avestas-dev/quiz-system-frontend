c84e6d2b51cd298f6f1a996288637cda6e3ab1c4
misio <kartofel6223@gmail.com>
2022-12-23 17:01:46 +0100

    edit time

--- src/sites/trainings/components/QuestionListItem.tsx
+++ src/sites/trainings/components/QuestionListItem.tsx
@@ -9,37 +9,32 @@ interface QuestionProps {
   id?: number
   question?: string
   withButtons?: boolean
-  tag?: string
+  updatedAt?: string
 }
 
 export const QuestionListItem = ({
   id,
   question,
   withButtons,
-  tag,
+  updatedAt,
 }: QuestionProps) => {
   const navigate = useNavigate()
+
+  const date = new Date(updatedAt!)
   return (
     <div
-      className="flex flex-col  bg-white border-1 border-gray-400 rounded-xl"
+      className="flex flex-col shadow-xl  bg-white border-2 border-gray-400 rounded-xl"
       onClick={() => {
         //navigate("/")
       }}
     >
       <div className="h-5/6 p-2 space-x-2">
         <div className="flex flex-row space-x-1 float-left">
-          <input
-            checked
-            onChange={() => {}}
-            className=" accent-green-400"
-            id="multi-choice-checkbox"
-            type="checkbox"
-          ></input>
-          <p>{tag}</p>
+          <h3>Last update: {date.toDateString()}</h3>
         </div>
         <div className="flex flex-row space-x-1 float-right border-2 text-[10px] border-gray-300">
           <AccessTimeIcon fontSize="small" />
-          <p className="mt-1">30 sekund</p>
+          <p className="mt-1">30 seconds</p>
         </div>
       </div>
       <div className="p-2 space-x-2">
